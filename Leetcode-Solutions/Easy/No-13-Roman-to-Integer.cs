b15322562ef9f6469c8287f0d118cfd52a76a4f0
martinh2018pu <55690426+martinh2018pu@users.noreply.github.com>
2022-09-15 16:11:25 +0300

    Manually uploaded files - sln, proj, No 13 Roman to Integer cs and Program cs

---
+++ Leetcode-Solutions/Easy/No-13-Roman-to-Integer.cs
@@ -0,0 +1,98 @@
+﻿namespace Leetcode_Solutions.Easy
+{
+    using System;
+
+    class No_13_Roman_to_Integer
+    {
+        public static void RomanToInt()
+        {
+            Console.WriteLine("Enter valid roman numeral in the range [1, 3999]: ");
+
+            string romanNumeral = Console.ReadLine();
+
+            romanNumeral += "*";
+
+            char[] arr = romanNumeral.ToCharArray();
+
+            int res = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == '*')
+                {
+                    break;
+                }
+
+                if (arr[i] == 'M')
+                {
+                    res += 1000;
+                }
+
+                if (arr[i] == 'D')
+                {
+                    res += 500;
+                }
+
+                if (arr[i] == 'C')
+                {
+                    if (arr[i + 1] == 'M')
+                    {
+                        res += 900;
+                        i++;
+                    }
+                    else if (arr[i + 1] == 'D')
+                    {
+                        res += 400;
+                        i++;
+                    }
+                    else
+                        res += 100;
+                }
+
+                if (arr[i] == 'L')
+                {
+                    res += 50;
+                }
+
+                if (arr[i] == 'X')
+                {
+                    if (arr[i + 1] == 'C')
+                    {
+                        res += 90;
+                        i++;
+                    }
+                    else if (arr[i + 1] == 'L')
+                    {
+                        res += 40;
+                        i++;
+                    }
+                    else
+                        res += 10;
+                }
+
+                if (arr[i] == 'V')
+                {
+                    res += 5;
+                }
+
+                if (arr[i] == 'I')
+                {
+                    if (arr[i + 1] == 'X')
+                    {
+                        res += 9;
+                        i++;
+                    }
+                    else if (arr[i + 1] == 'V')
+                    {
+                        res += 4;
+                        i++;
+                    }
+                    else
+                        res += 1;
+                }
+            }
+
+            Console.WriteLine("Your roman numeral, converted into an integer is equal to: {0}", res);
+        }
+    }
+}
---
+++ Leetcode-Solutions/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Leetcode_Solutions
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            // Press Ctrl + F5 to start.
+            // Choose the leetcode problem which you want: 
+            Easy.No_13_Roman_to_Integer.RomanToInt();
+        }
+    }
+}

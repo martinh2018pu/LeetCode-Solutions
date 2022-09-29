f4de8c015f4973626f9110f07e453da7e9f3735f
martinh2018pu <55690426+martinh2018pu@users.noreply.github.com>
2022-09-29 06:12:31 +0300

    Added No-412-Fizz-Buzz.cs

---
+++ Leetcode-Solutions/Easy/No-412-Fizz-Buzz.cs
@@ -0,0 +1,35 @@
+﻿namespace Leetcode_Solutions.Easy
+{
+    using System.Collections.Generic;
+
+    class No_412_Fizz_Buzz
+    {
+        // TODO: Create test case and write to console methods. 
+
+        public IList<string> FizzBuzz(int n)
+        {
+            string[] arr = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                if ((i + 1) % 3 == 0)
+                {
+                    if ((i + 1) % 5 == 0)
+                    {
+                        arr[i] = "FizzBuzz";
+                    }
+                    else
+                        arr[i] = "Fizz";
+                }
+                else if ((i + 1) % 5 == 0)
+                {
+                    arr[i] = "Buzz";
+                }
+                else
+                    arr[i] = (i + 1).ToString();
+            }
+
+            return arr;
+        }
+    }
+}
